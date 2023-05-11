78fbeacd37af1f0ea762917e888cc9049f8d93bd
Manu Cornet <m@ma.nu>
2023-05-11 21:21:25 +0000

    Pivoting from trying to use go/ in the URL, into more of a search engine in the popup

--- common.js
+++ common.js
@@ -1,3 +1,16 @@
+let routingTable;
+
+const ROUTING_TABLE_STORAGE_KEY = 'routing';
+
+const loadRoutingTable = () => {
+  chrome.storage.local.get(ROUTING_TABLE_STORAGE_KEY, (data) => {
+    console.log('Loaded');
+    console.log(data);
+    routingTable = JSON.parse(data[ROUTING_TABLE_STORAGE_KEY]);
+    console.log(routingTable);
+  });
+};
+
 const refreshRoutingTable = (url) => {
   return fetch(url).then((res) => {
     return res.text().then((data) => {
@@ -12,11 +25,13 @@ const refreshRoutingTable = (url) => {
       return new Promise((resolve, reject) => {
         try {
           const parsed = JSON.parse(data);
-          chrome.storage.local.set({'routing': data}, () => {
+          routingTable = parsed;
+          chrome.storage.local.set({ROUTING_TABLE_STORAGE_KEY: data}, () => {
             if (chrome.runtime.lastError) {
               console.log('Local storage failure');
               reject(chrome.runtime.lastError);
             } else {
+              console.log('Updated storage');
               resolve(parsed);
             }
           });
@@ -27,4 +42,42 @@ const refreshRoutingTable = (url) => {
       });
     });
   });
+};
+
+const search = (query) => {
+  if (!routingTable) {
+    console.log('No routing table');
+    return [];
+  }
+  query = query.toLowerCase();
+  if (!query.trim()) {
+    console.log('Empty query');
+    return [];
+  }
+  if (query in routingTable) {
+    // Exact match, return just that one.
+    console.log('Match for ', routingTable[query]);
+    return [[query, routingTable[query]]];
+  }
+  const filtered = [];
+  for (const key in routingTable) {
+    if (key.includes(query)) {
+      filtered.push([key, routingTable[key]]);
+    }
+  }
+  console.log('Filtered', filtered);
+  return filtered;
+};
+
+const formatSearchResult = (result, userQuery) => {
+  let output = '';
+  output += '<div class="result">';
+  output += '  <div class="result-key">';
+  output +=      result[0];
+  output += '  </div>';
+  output += '  <div class="result-target">';
+  output +=      result[1][0];
+  output += '  </div>';
+  output += '</div>';
+  return output;
 };
--- popup.js
+++ popup.js
@@ -1,6 +1,24 @@
+const searchField = document.querySelector('#search-input');
 const inputField = document.querySelector('#routing-input');
 const saveButton = document.querySelector('#save');
-inputField.focus();
+const resultsContainer = document.querySelector('#results');
+searchField.focus();
+
+loadRoutingTable();
+
+searchField.addEventListener('input', (e) => {
+  console.log(routingTable);
+  const query = e.target.value;
+  const results = search(query);
+
+  resultsContainer.innerHTML = '';
+  console.log('Results', results);
+  for (const result of results) {
+    let resultEl = document.createElement('div');
+    resultEl.innerHTML = formatSearchResult(result);
+    resultsContainer.appendChild(resultEl);
+  }
+});
 
 saveButton.addEventListener('click', (event) => {
   refreshRoutingTable(inputField.value).then((table) => {
