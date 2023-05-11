517f6ec8327a21408486195f4da3c78f13bd0fdc
Manu Cornet <m@ma.nu>
2023-05-11 05:46:27 +0000

    Refresh data when saving the new routing URL

--- common.js
+++ common.js
@@ -1,7 +1,17 @@
 const refreshRoutingTable = (url) => {
-  fetch(url).then((res) => {
+  return fetch(url).then((res) => {
+    console.log(res);
     res.text().then((data) => {
       console.log(data);
+      return new Promise((resolve, reject) => {
+        chrome.storage.local.set({'routing': data}, () => {
+          if (chrome.runtime.lastError) {
+            reject(chrome.runtime.lastError);
+          } else {
+            resolve();
+          }
+        });
+      });
     });
   });
 };
--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
-document.querySelector('#save').addEventListener('click', (event) => {
-  refreshRoutingTable(document.querySelector('#routing-input').value);
+const inputField = document.querySelector('#routing-input');
+const saveButton = document.querySelector('#save');
+inputField.focus();
+
+saveButton.addEventListener('click', (event) => {
+  refreshRoutingTable(inputField.value).then(() => {
+    saveButton.classList.add('success');
+    saveButton.textContent = 'Success!';
+  });
 });
